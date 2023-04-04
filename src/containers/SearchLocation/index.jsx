b03609eab3096511413a8d823d6ecc95f6d06f0b
ayse8888 <aysebasar@yahoo.com>
2023-04-05 00:39:03 +0300

    error handling

--- src/containers/SearchLocation/index.jsx
+++ src/containers/SearchLocation/index.jsx
@@ -8,46 +8,66 @@ const SearchLocation = () => {
     const [city, setCity] = useState('Rome');
     const [location, setLocation] = useState('');
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const url = `${BASE_URL}/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
     const searchLocation = (event) => {
         setLoading(true)
-        axios.get(url).then((response) => {
-            setCity(response.data);
-            setLoading(false)
-            console.log(response.data);
-        });
+        axios
+            .get(url)
+            .then((response) => {
+                setCity(response.data);
+                setLoading(false)
+                setError(null);
+                console.log(response.data);
+            })
+            .catch((err) => {
+                setError(err);
+                console.log('err', err?.response?.data?.message);
+            });
         setLocation('');
     };
 
+    const getErrorView = () => {
+        return (
+            <div>
+                Oh no! Something went wrong. {error?.response?.data?.message}
+                {/* <button onClick={() => searchLocation()}>Try again</button> */}
+            </div>
+        );
+    };
+
+    const loadingSpinner = () => {
+        return(
+            <div>Loading</div>
+        )
+    }
+
+    if (error) return getErrorView();
+    if (loading) return loadingSpinner();
+
     return (
         <div>
-            {loading ? (
-                <p>Loading</p>
-            ) : (
-                <>
-                    <Input
-                        type="text"
-                        placeholder="Enter City.."
-                        value={location}
-                        onChange={(event) => setLocation(event.target.value)}
-                    />
-                    <Button onClick={searchLocation} />
-                    <h1 className="city">{city?.name}</h1>
-                    <p className="city">Temperature: {city?.main?.temp.toFixed()} °C</p>
-                    <p className="city">Feels Like: {city?.main?.feels_like.toFixed()} °C</p>
-                    <p className="city">Humidity: {city?.main?.humidity.toFixed()} %</p>
-                    <p className="city">Wind Speed: {city?.wind?.speed.toFixed()} km/h</p>
-                    {city.weather ? <p>Description: {city.weather[0].description}</p> : null}
-                    {city.weather ? <p>{city.weather[0].main}</p> : null}
-                    {city.weather && (
-                        <img
-                            src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
-                            alt=""
-                        />
-                    )}
-                </>
+            <Input
+                type="text"
+                placeholder="Enter City.."
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+            />
+            <Button onClick={searchLocation} />
+            <h1 className="city">{city?.name}</h1>
+            <p className="city">Temperature: {city?.main?.temp.toFixed()} °C</p>
+            <p className="city">Feels Like: {city?.main?.feels_like.toFixed()} °C</p>
+            <p className="city">Humidity: {city?.main?.humidity.toFixed()} %</p>
+            <p className="city">Wind Speed: {city?.wind?.speed.toFixed()} km/h</p>
+            {city.weather ? <p>Description: {city.weather[0].description}</p> : null}
+            {city.weather ? <p>{city.weather[0].main}</p> : null}
+            {city.weather && (
+                <img
+                    src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+                    alt=""
+                />
             )}
         </div>
     );
