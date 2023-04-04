87579c861ad68638e77da4ca0f6bba65bc4be0b6
ayse8888 <aysebasar@yahoo.com>
2023-04-05 00:14:55 +0300

    loading status added

--- src/containers/SearchLocation/index.jsx
+++ src/containers/SearchLocation/index.jsx
@@ -5,14 +5,17 @@ import axios from 'axios';
 import { API_KEY, BASE_URL } from '../../constants/privateKeys';
 
 const SearchLocation = () => {
-    const [city, setCity] = useState('');
+    const [city, setCity] = useState('Rome');
     const [location, setLocation] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const url = `${BASE_URL}/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
     const searchLocation = (event) => {
+        setLoading(true)
         axios.get(url).then((response) => {
             setCity(response.data);
+            setLoading(false)
             console.log(response.data);
         });
         setLocation('');
@@ -20,25 +23,31 @@ const SearchLocation = () => {
 
     return (
         <div>
-            <Input
-                type="text"
-                placeholder="Enter City.."
-                value={location}
-                onChange={(event) => setLocation(event.target.value)}
-            />
-            <Button onClick={searchLocation} />
-            <h1 className="city">{city?.name}</h1>
-            <p className="city">Temperature: {city?.main?.temp.toFixed()} °C</p>
-            <p className="city">Feels Like: {city?.main?.feels_like.toFixed()} °C</p>
-            <p className="city">Humidity: {city?.main?.humidity.toFixed()} %</p>
-            <p className="city">Wind Speed: {city?.wind?.speed.toFixed()} km/h</p>
-            {city.weather ? <p>Description: {city.weather[0].description}</p> : null}
-            {city.weather ? <p>{city.weather[0].main}</p> : null}
-            {city.weather && (
-                <img
-                    src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
-                    alt=""
-                />
+            {loading ? (
+                <p>Loading</p>
+            ) : (
+                <>
+                    <Input
+                        type="text"
+                        placeholder="Enter City.."
+                        value={location}
+                        onChange={(event) => setLocation(event.target.value)}
+                    />
+                    <Button onClick={searchLocation} />
+                    <h1 className="city">{city?.name}</h1>
+                    <p className="city">Temperature: {city?.main?.temp.toFixed()} °C</p>
+                    <p className="city">Feels Like: {city?.main?.feels_like.toFixed()} °C</p>
+                    <p className="city">Humidity: {city?.main?.humidity.toFixed()} %</p>
+                    <p className="city">Wind Speed: {city?.wind?.speed.toFixed()} km/h</p>
+                    {city.weather ? <p>Description: {city.weather[0].description}</p> : null}
+                    {city.weather ? <p>{city.weather[0].main}</p> : null}
+                    {city.weather && (
+                        <img
+                            src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+                            alt=""
+                        />
+                    )}
+                </>
             )}
         </div>
     );
